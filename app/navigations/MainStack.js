8911105114821beeee4a9936e94a636400cbec71
jung0115 <jungim7490@gmail.com>
2023-10-04 21:29:09 +0900

    [FEAT] #08 - 마이페이지 구현 중중

--- app/navigations/MainStack.js
+++ app/navigations/MainStack.js
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
 import { ThemeContext } from 'styled-components/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { TransDetail, DoranHome } from '../screens';
+import { TransDetail, DoranHome, MyPage } from '../screens';
 
 const Stack = createStackNavigator();
 
@@ -11,12 +11,13 @@ const MainStack = () => {
   // 임시로 모우미 세부 페이지를 메인으로 해둠. 모우미 메인 만들면 initialRouteName 부분 바꿀 것
   return (
     <Stack.Navigator
-      initialRouteName="TransDetail"
+      initialRouteName="MyPage"
       screenOptions={{ headerShown: false }}  
     >
       {/*<Stack.Screen name="Main" component={Main} />*/}
       <Stack.Screen name="TransDetail" component={TransDetail}/>
       <Stack.Screen name="Doran" component={DoranHome}/>
+      <Stack.Screen name="MyPage" component={MyPage}/>
     </Stack.Navigator>
   );
 };
